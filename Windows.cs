38660b9da3da9587af90cf41db1a3aedcc9b96a0
dadigno97 <daviderng@gmail.com>
2020-05-14 10:00:01 +0200

    aggiunto gitignore

--- Windows.cs
+++ Windows.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace Gioco_generico
 {
@@ -16,12 +14,11 @@ namespace Gioco_generico
         Vector2 displayPos;
         Button exit;
         Rectangle rect;
-        public Windows(Game1 _game, GraphicsDeviceManager _graphics, ContentManager _content, Vector2 displayPos, string texture, double scale = 1) : base(_game, _graphics, _content)
+        public Windows(Game1 _game, GraphicsDeviceManager _graphics, ContentManager _content, Vector2 displayPos, string texture, double scaleX = 1, double scaleY = 1) : base(_game, _graphics, _content)
         {
-            this.displayPos = displayPos;
+            this.displayPos = displayPos;   //finestra centrata
             background = _content.Load<Texture2D>(texture);
-            rect = new Rectangle((int)displayPos.X, (int)displayPos.Y, (int)(background.Width * scale),(int)(background.Height * scale));
-
+            rect = new Rectangle((int)displayPos.X, (int)displayPos.Y, (int)(background.Width * scaleX),(int)(background.Height * scaleY));
             widget = new List<Object>();
         }
 

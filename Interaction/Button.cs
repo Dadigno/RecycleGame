8ea1bbca9cda41214e6bc59577fbbc83e68aaa3c
dadigno97 <daviderng@gmail.com>
2020-05-17 14:17:26 +0200

    la classe button non deriva piu da sprite. Ho implementato lo "sprite" dentro button per fare prima

--- Interaction/Button.cs
+++ Interaction/Button.cs
@@ -2,10 +2,11 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Gioco_generico
 {
-    public class Button : Sprite
+    public class Button
     {
         private MouseState _currentMouse;
         private MouseState _previousMouse;
@@ -13,21 +14,33 @@ namespace Gioco_generico
         private bool _isPressed;
         public bool _isActive { get; set; }
         public EventHandler Click;
-        public Button(Game1 _game, GraphicsDeviceManager _graphics, ContentManager _content, String texture, Vector2 initDisplayPos, double scale = 1, bool state = true) : base(_game, _graphics, _content, texture, new Vector2(0, 0), initDisplayPos, scale)
+
+        Texture2D texture;
+        Rectangle rect;
+        Color colourTex = Color.White;
+        Vector2 origin;
+        public Button(Game1 _game, GraphicsDeviceManager _graphics, ContentManager _content, String texture, Vector2 initDisplayPos, double scale = 1, bool state = true)
         {
+            if(texture != "")
+            {
+                this.texture = _content.Load<Texture2D>(texture);
+                rect = new Rectangle((int)initDisplayPos.X - (int)(this.texture.Width * scale) / 2, (int)initDisplayPos.Y - (int)(this.texture.Height * scale) / 2, (int)(this.texture.Width * scale), (int)(this.texture.Height * scale));
+            }
+            else
+            {
+                rect = new Rectangle((int)initDisplayPos.X, (int)initDisplayPos.Y, 1, 1);
+            }
+            origin = new Vector2(0, 0);
             _isActive = state;
+
         }
 
         public new void Draw()
         {
-            /*colourTex = Color.White;
+            colourTex = Color.White;
             if (_isPressed || !_isActive)
                 colourTex = Color.Gray;
-            
-            isVisible = true;
-            */
-
-            Draw(true);
+            ConstVar.sb.Draw(texture, rect, null, colourTex, 0, origin, new SpriteEffects(), 0);
         }
 
         public void update()
@@ -35,12 +48,17 @@ namespace Gioco_generico
             _previousMouse = _currentMouse;
             _currentMouse = Mouse.GetState();
             var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
-            _isPressed = false;
 
             if (mouseRectangle.Intersects(rect) && _currentMouse.LeftButton == ButtonState.Pressed)
             {
                 _isPressed = true;
             }
+            else
+            {
+                _isPressed = false;
+            }
+
+
             if (mouseRectangle.Intersects(rect) && _currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed)
             {
                 if (_isActive)

28ee72538b9ee615d32c4f0525bc1503316557c4
dadigno97 <daviderng@gmail.com>
2020-06-11 09:31:09 +0200

    Aggiunta classe per gestire gli automatismi

---
+++ GameController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gioco_generico
+{
+    public class GameController
+    {
+
+        public GameController()
+        {
+
+        }
+
+    }
+}

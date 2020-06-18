6bd97b9517a7e437eaa5f43e78668ead78f56d2c
dadigno97 <daviderng@gmail.com>
2020-06-18 12:39:27 +0200

    aggiunti simboli

--- Interaction/Vocabulary.cs
+++ Interaction/Vocabulary.cs
@@ -45,6 +45,7 @@ namespace Gioco_generico.Interaction
 
         //Sound effect
         SoundEffect effectScrollPage;
+
         //Draw page
         delegate void DrawPage();
         DrawPage _DrawPage;
@@ -73,23 +74,48 @@ namespace Gioco_generico.Interaction
             effectScrollPage = _content.Load<SoundEffect>("soundEffect/turnPage");
 
             simbols = new List<ConstVar.Symbol>();
-            ConstVar.Symbol s1 = new ConstVar.Symbol( new Sprite(_game, _graphics, _content, "simboli/raee", new Vector2(0, 0), new Vector2(0,0), 0.25f), "Il logo Raee indica rifiuti elettrici o elettronici che non devono per nessuna ragione essere gettati tra i rifiuti generici ma smaltiti in appositi contenitori o piattaforma ecologica.");
-            simbols.Add(s1);
-            ConstVar.Symbol s2 = new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/appiattire", new Vector2(0, 0), new Vector2(0, 0), 0.4f), "Appiattire dopo l'uso invita a comprimere i contenitori per ridurne il volume e l'impatto ambientale.");
-            simbols.Add(s2);
-            ConstVar.Symbol s3 = new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/cestino", new Vector2(0, 0), new Vector2(0, 0), 0.25f), "Il cestino dei rifiuti indica che l'oggetto non va disperso nell'ambiente dopo l'uso. Se avete in mano un prodotto riciclabile ma siete lontani dall'apposito contenitore, il logo invita a conservarlo anziche' abbandonarlo in luoghi pubblici.");
-            simbols.Add(s3);
-            ConstVar.Symbol s4 = new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/moebius", new Vector2(0, 0), new Vector2(0, 0), 0.25f), "Il simbolo di riciclaggio e' il simbolo internazionale che indica il riciclaggio dei rifiuti. E' composto da tre frecce che formano un nastro dinMobius.");
-            simbols.Add(s4);
-            ConstVar.Symbol s5 = new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/grune", new Vector2(0, 0), new Vector2(0, 0), 0.25f), "Punto verde individua un sistema di smaltimento degli imballaggi, ma non fornisce alcuna informazione aggiuntiva sulla riciclabilita' del prodotto.");
-            simbols.Add(s5);
-            ConstVar.Symbol s6 = new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/compostabile", new Vector2(0, 0), new Vector2(0, 0), 0.40f), "Il simbolo compostabili e' uno dei marchi degli organismi certificatori accreditati che attestano la certificazione della biodegradabilita' e della compostabilita'");
-            simbols.Add(s6);
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/raee", new Vector2(0, 0), new Vector2(0,0), 0.25f), "Il logo Raee indica rifiuti elettrici o elettronici che non devono per nessuna ragione essere gettati tra i rifiuti generici ma smaltiti in appositi contenitori o piattaforma ecologica."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/appiattire", new Vector2(0, 0), new Vector2(0, 0), 0.4f), "Appiattire dopo l'uso invita a comprimere i contenitori per ridurne il volume e l'impatto ambientale."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/cestino", new Vector2(0, 0), new Vector2(0, 0), 0.25f), "Il cestino dei rifiuti indica che l'oggetto non va disperso nell'ambiente dopo l'uso. Se avete in mano un prodotto riciclabile ma siete lontani dall'apposito contenitore, il logo invita a conservarlo anziche' abbandonarlo in luoghi pubblici."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/moebius", new Vector2(0, 0), new Vector2(0, 0), 0.25f), "Il simbolo di riciclaggio e' il simbolo internazionale che indica il riciclaggio dei rifiuti. E' composto da tre frecce che formano un nastro dinMobius."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/grune", new Vector2(0, 0), new Vector2(0, 0), 0.25f), "Punto verde individua un sistema di smaltimento degli imballaggi, ma non fornisce alcuna informazione aggiuntiva sulla riciclabilita' del prodotto."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/compostabile", new Vector2(0, 0), new Vector2(0, 0), 0.40f), "Il simbolo compostabili e' uno dei marchi degli organismi certificatori accreditati che attestano la certificazione della biodegradabilita' e della compostabilita'"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/PET-01", new Vector2(0, 0), new Vector2(0, 0), 0.08f), "Polietilentereftalato, un materiale leggero riciclabile al 100% utilizzato principalmente a fini alimentari"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/2_PEHD", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Polietilene ad alta densita', anche detto HDPE, una resina termoplastica usata per sacchetti della spesa e contenitori di cibo e saponi"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/3_PVC", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Polivinilcloruro, sostanza dalla consistenza gommosa usata soprattutto per bottiglie, tubature, giocattoli"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/4_PELD", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Anche detto LDPE, Polietilene a bassa densita': plastica molto utilizzata per sacchi e sacchetti molto morbidi"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/05-PP", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Polipropilene, termoplastica molto usata per gli oggetti di arredamento, contenitori e flaconi"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/06-PS", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Polistirolo o polistirene, usato soprattutto come isolante nel settore edile e negli imballaggi delle merci"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/07-O", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Con queste scritte si identificano tutti quei materiali che non rientrano nelle categorie precedenti e che non sono riciclabili"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/11-MiNh", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Identifica un le batterie di tipo Nichel Metallo Idruro"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/12-Lithium", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Identifica un tipo di batterie, batterie al Litio"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/13-HgO", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Identifica le batterie a ossido di argento"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/21-PAP", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Carta e cartone, puo' presentare anche codici 20,22"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/40_FE", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Acciaio: metallo utilizzato per contenitori e scatolette"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/51-FOR", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Legno, in particolare il codice 51 identifica il sughero, altri da 52 a 59 altri tipi di legno"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/60_TEX", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Materiali tessili, in particolare 60 corrisponde al cotone (vestiario). Dal 61 al 69 altri materiali tessili. IL cotone ad uso medicale non e' differenziato"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/70-GL", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Vetro trasparente e incolore, altri tipi di vetro vanno dal 73 al 79"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/72-GL", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Vetro di colore marrone, altri tipi di vetro vanno dal 73 al 79"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/ABS", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Acrylonitrile butadiene styrene,  NON e' plastica: e' un materiale antiurto utilizzato per utensili da cucina, parti plastiche di elettrodomestici, caschi motociclistici e paraurti e altri particolari delle auto "));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/ALU_41", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Alluminio: metallo utilizzato per lattine, scatolette e contenitori per alimenti"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/20-PAP", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Cartone ondulato, scatole per imballaggi"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/9-ALCALINE", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Batterie di tipo alcaline"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/8-PIOMBO", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Batterie di tipo al piombo"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/50-FOR", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Legno generico, altri da 52 a 59 altri tipi di legno specifici"));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/61-TEX", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Materiali tessili,  in particolare Juta. Dal 62 al 69 altri materiali tessili."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/PAP_22", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Carta: carta di gionale sacchetti libri ecc, dal 23 al 39 altri tipi di carta."));
+            simbols.Add(new ConstVar.Symbol(new Sprite(_game, _graphics, _content, "simboli/Vetro71", new Vector2(0, 0), new Vector2(0, 0), 0.70f), "Vetro di colore verde, altri tipi di vetro vanno dal 73 al 79"));
+
+
             titlefont = _content.Load<SpriteFont>("Fonts/bigFont");
             font = _content.Load<SpriteFont>("Fonts/fontSimbol");
             _DrawPage = DrawPage1;
         }
-
+        /// <summary>
+        /// Vai alla pagina precedente
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PreviousPage(object sender, EventArgs e)
         {
 
@@ -149,11 +175,10 @@ namespace Gioco_generico.Interaction
 
             foreach (var simbol in simbols)
             {
-
                 int i = simbols.IndexOf(simbol);
-                int offsetx = offset * (i % 5);
-                int offsety = offset * (i / 5);
-                simbol.symbol.setPos((int)(background.getPos().X + offsetx - background.getRect().Width * 0.30f), (int)(background.getPos().Y  + offsety - background.getRect().Height * 0.25f));
+                int offsetx = offset * (i % 6);
+                int offsety = offset * (i / 6);
+                simbol.symbol.setPos((int)(background.getPos().X + offsetx - background.getRect().Width * 0.40f), (int)(background.getPos().Y  + offsety - background.getRect().Height * 0.28f));
                 simbol.symbol.Draw(true);
             }
 
@@ -164,7 +189,7 @@ namespace Gioco_generico.Interaction
                 if (Intersects(mouseRectangle, simbol.symbol.getRect()))
                 {
                     SpeechBubble speechBubble = new SpeechBubble(_game, _graphics, _content, "bubble/bubble-narrator", "Fonts/speechFont");
-                    speechBubble.setPos(mouseRectangle.X + speechBubble.getRect().Width / 2, mouseRectangle.Y - speechBubble.getRect().Height / 2);
+                    speechBubble.setPos(mouseRectangle.X + (int)(speechBubble.getRect().Width * 0.25f), mouseRectangle.Y - speechBubble.getRect().Height / 2);
                     speechBubble.StaticDraw(simbol.description);
                 }
             }
@@ -183,6 +208,16 @@ namespace Gioco_generico.Interaction
             ConstVar.sb.DrawString(titlefont, title, new Vector2(background.getPos().X - titlefont.MeasureString(title).X / 2, background.getPos().Y - background.getRect().Height * 0.43f), Color.Black);
             foreach (var button in arrows)
                 button.Draw();
+
+            int offset = 80;
+            foreach (var obj in ConstVar.allObjects)
+            {
+                int i = ConstVar.allObjects.IndexOf(obj);
+                int offsetx = offset * (i % 7);
+                int offsety = offset * (i / 7);
+                obj.setPos((int)(background.getPos().X + offsetx - background.getRect().Width * 0.39f), (int)(background.getPos().Y + offsety - background.getRect().Height * 0.32f));
+                obj.Draw(true);
+            }
         }
 
     }

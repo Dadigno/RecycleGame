a26f071bffbd88e8a9ebd33959f22ec7490da4cd
dadigno97 <daviderng@gmail.com>
2020-06-18 09:43:17 +0200

    aggiunta nuova funzione per fare una nuvoletto con il testo che non scorre

--- Sprites/SpeechBubble.cs
+++ Sprites/SpeechBubble.cs
@@ -32,12 +32,12 @@ namespace Gioco_generico
         private SoundEffect speechSound;
         SoundEffectInstance instance;
 		float border;
+        bool isStatic;
         public SpeechBubble(Game1 _game, GraphicsDeviceManager _graphics, ContentManager _content, String nameTex, string font, float scale = 1) : base(_game, _graphics, _content, nameTex, new Vector2(0,0), new Vector2(0,0), scale) 
         {
             this.font = _content.Load<SpriteFont>(font);
             scrollingTime = SCROLL_VEL;
             bubbleTime = BUBBLE_DELAY;
-
             //Load effect
             speechSound = _content.Load<SoundEffect>("soundEffect/speech-sound");
             instance = speechSound.CreateInstance();
@@ -76,6 +76,9 @@ namespace Gioco_generico
             }
         }
         float row = 1;
+        /// <summary>
+        /// Mostra la nuvoletta con lo scorrimento del testo e il suono della voce
+        /// </summary>
         public new bool Draw()
         {
             if (text != "")
@@ -128,6 +131,40 @@ namespace Gioco_generico
 
             }
             return false;
+        }
+
+        /// <summary>
+        /// Mostra la nuvoletta ma senza lo scorrimento del testo
+        /// </summary>
+        public new void StaticDraw(string text)
+        {
+            Text = text;
+            while (l.Count() > 0)
+            {
+                temp += l[0];
+                float a = font.MeasureString(temp.Replace("\r\n", "")).X / border;
+                if (a > row)
+                {
+                    if (l[0] != ' ' && l[1] != ' ')
+                    {
+                        temp += '-';
+                    }
+                    if (l[1] == ' ')
+                    {
+                        l.RemoveAt(1);
+                    }
+
+                    temp += Environment.NewLine;
+                    row++;
+                }
+                l.RemoveAt(0);
+            }
+            row = 1;
+            if (show)
+            {
+                base.Draw(true);
+                ConstVar.sb.DrawString(font, temp, new Vector2(rect.X - texture.Width * 0.45f, rect.Y - texture.Height * 0.43f), PenColour);
+            }
         }
             
     }
